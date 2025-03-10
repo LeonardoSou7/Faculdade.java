bb721ddf03e6559e863fac6e3387ac9ed48bef6f
LeonardoSou7 <161030579+LeonardoSou7@users.noreply.github.com>
2025-03-10 08:54:42 -0300

    Add files via upload

---
+++ Faculdade.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.faculdade;
+
+import java.util.Random;
+
+/**
+ *
+ * @author alunolab03
+ */
+public class Faculdade {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        System.out.println("Hello World!");
+        
+        
+        
+        
+    
+    
+      
+        
+        
+
+    }
+}
