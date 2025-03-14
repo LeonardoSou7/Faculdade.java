77e335a20c6d5ba9e358bd0e94bd81b03c017e26
LeonardoSou7 <161030579+LeonardoSou7@users.noreply.github.com>
2025-03-14 00:36:09 -0300

    Add files via upload

--- Aluno.java
+++ Aluno.java
@@ -1,31 +1,139 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.faculdade;
 
+
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.Random;
 
-/**
- *
- * @author alunolab03
- */
+
 public class Aluno {
-   
-    public String Nome = "Cristiano";
-    public String Curso = "Ciencias da Computação";
-    public String Turma = "CC3M";
-    public int Periodo = 3;
-    public double Nota1B = 8.0;
-    public double Nota2B = 6.5;
-    public double NotaFinal = (Nota1B + Nota2B)/2;
+    private static final AtomicInteger contadorAlunos = new AtomicInteger(0);
+    private String matricula;
+    private String nome;
+    private String curso;
+    private String turma;
+    private int periodo;
+    private double nota1B;
+    private double nota2B;
+    private double notaFinal;
     
-    int ano = 2025;
-    Random random = new Random();
-    int numeroAleatorio = random.nextInt(10000);
-    String numeroFinal = String.format("%04d", numeroAleatorio);
-    public String Matricula = ano + numeroFinal;
     
+    private String gerarMatricula() {
+        int ano = java.time.Year.now().getValue();
+        int aleatorio = new Random().nextInt(10000);
+        return String.format("%d%04d", ano, aleatorio);
+    }
+    
+    
+    public double calcularFinal() {
+        return (nota1B + nota2B) / 2;
+    }
+    
+    
+    public void passar(String novaTurma, int novoPeriodo) {
+        this.turma = novaTurma;
+        this.periodo = novoPeriodo;
+    }
+
+    
+    public static int getQuantidadeAlunos() {
+        return contadorAlunos.get();
+    }
+    
+    
+    public void setNome(String nome) {
+        this.nome = nome.toUpperCase();
+    }
+    
+    
+    public Aluno() {
+        this.matricula = gerarMatricula();
+        contadorAlunos.incrementAndGet();
+    }
+    
+    
+    public Aluno(String nome, String curso, String turma, int periodo, double nota1B, double nota2B) {
+        this.matricula = gerarMatricula();
+        this.nome = nome.toUpperCase();
+        this.curso = curso;
+        this.turma = turma;
+        this.periodo = periodo;
+        this.nota1B = nota1B;
+        this.nota2B = nota2B;
+        this.notaFinal = calcularFinal();
+        contadorAlunos.incrementAndGet();
+    }
+    
+    
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public int getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(int periodo) {
+        this.periodo = periodo;
+    }
+
+    public double getNota1B() {
+        return nota1B;
+    }
+
+    public void setNota1B(double nota1B) {
+        this.nota1B = nota1B;
+        this.notaFinal = calcularFinal();
+    }
+
+    public double getNota2B() {
+        return nota2B;
+    }
+
+    public void setNota2B(double nota2B) {
+        this.nota2B = nota2B;
+        this.notaFinal = calcularFinal();
+    }
+
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+    
+    
+    public void imprimir() {
+        System.out.println("-----------------------------------");
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("Nome: " + nome);
+        System.out.println("Curso: " + curso);
+        System.out.println("Turma: " + turma);
+        System.out.println("Período: " + periodo);  
+        System.out.println("Nota 1º Bimestre: " + nota1B);
+        System.out.println("Nota 2º Bimestre: " + nota2B);
+        System.out.println("Nota Final: " + notaFinal);
+        System.out.println("-----------------------------------");
+    }
+    
+
     
     
 }
--- Faculdade.java
+++ Faculdade.java
@@ -1,30 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.faculdade;
 
-import java.util.Random;
-
-/**
- *
- * @author alunolab03
- */
 public class Faculdade {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Aluno aluno1 = new Aluno("Leonardo Rosa", "Ciencia da Computação", "CC3M", 3, 7.6, 7.3);
+        Aluno aluno2 = new Aluno("Arthur Prates", "Ciencia da Computação", "CC3M", 3, 9.2, 8.3);
+        Aluno aluno3 = new Aluno("Gabriel Aguilar", "Ciencia da Computação", "CC3M", 3, 8.0, 8.4);
         
-        System.out.println("Hello World!");
+        System.out.println("Números de babuínos matriculados: " + Aluno.getQuantidadeAlunos());
+        
+        aluno1.imprimir();
+        aluno2.imprimir();
+        aluno3.imprimir();
+        
+        Aluno aluno4 = new Aluno("Matheus Presidente", "Ciencia da Computação", "CC3M", 3, 6.6, 6.0);
+        Aluno aluno5 = new Aluno("João Pinto Presidente", "Ciencia da Computação", "CC3M", 16, 1.0, 0.5);
+        
+        System.out.println("Números de babuínos matriculados: " + Aluno.getQuantidadeAlunos());
+        
+        System.out.println("Um ano depois:");
         
         
+        aluno1.passar("CC4M", 4);
+        aluno2.passar("CC4M", 4);
+        aluno3.passar("CC4M", 4);
+        aluno4.passar("CC4M", 4);
+        aluno5.passar("CC3M", 17);
         
-        
-    
-    
-      
-        
-        
-
+        aluno1.imprimir();
+        aluno2.imprimir();
+        aluno3.imprimir();
+        aluno4.imprimir();
+        aluno5.imprimir();
+   
     }
+    
 }
